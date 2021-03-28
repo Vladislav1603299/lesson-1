02c17b53d2e4a3a68980c0d8703b52ff98081814
lesson1 <abinin99@bk.ru>
2021-03-28 12:07:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+alert("1 задание");
+var degreesCelsius = +prompt("Введите температуры в градксах по Цельсию");
+degreesFaringate = (9 / 5) * degreesCelsius + 32;
+alert("Получаем " + degreesFaringate + " по Фаренгейту");
+
+alert("2 задание");
+
+var name = "Василий";
+var admin = "Василий";
+alert(admin);
+
+alert("3 задание");
+alert("Чему равно Чему будет равно JS-выражение 1000 + '108'?");
+alert("Ответ: 1000108");
+alert("4 задание просмотрел")
+
+
+
